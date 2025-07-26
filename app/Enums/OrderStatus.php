9a66e619c2419848d6d83a660a748ed12cbe8588
yngcidw <alcidesdaconceicao03@gmail.com>
2025-07-26 19:53:13 +0200

    Enums creation

--- app/Enums/OrderStatus.php
+++ app/Enums/OrderStatus.php
@@ -4,5 +4,9 @@ namespace App\Enums;
 
 enum OrderStatus
 {
-    //
+    case PENDING = "pendente";
+    case PROCESSING = "processando";
+    case CANCELLED = "cancelado";
+    case COMPLETED = "concluido";
+    
 }
---
+++ app/Enums/Priority.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+enum Priority
+{
+    case LOW = "baixa";
+    case NORMAL = "normal";
+    case HIGH = "alta";
+    case MAX = "maxima";
+
+}
