29d686fe2102b6b7f36714bb3cf391394b511ae9
Estêvão Amâncio Cuave <estevaocuave822@gmail.com>
2025-08-04 11:43:26 +0200

    set repo

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+Route::get('/', [CategoryController::class, 'index'])->name("Hello");
